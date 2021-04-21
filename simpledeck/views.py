18573e00a9253835247b2935c905722f6fb85686
Andrii Yurchuk <ay@mntw.re>
2021-04-21 17:33:27 +0200

    Make mypy happy

--- simpledeck/views.py
+++ simpledeck/views.py
@@ -2,5 +2,5 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def handler404(request: HttpRequest, exception=None) -> HttpResponse:
+def handler404(request: HttpRequest, exception: Exception = None) -> HttpResponse:
     return render(request, "404.html")
