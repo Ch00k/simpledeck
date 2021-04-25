d1d10a93f7148be5df8d399a6f52ce4d23b809dc
Andrii Yurchuk <ay@mntw.re>
2021-04-25 20:14:01 +0200

    Add SIMPLEDECK_DB_PATH env. variable (#7)

--- simpledeck/settings.py
+++ simpledeck/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "simpledeck.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": env.path("SIMPLEDECK_DB_PATH", str(BASE_DIR / "db.sqlite3")),
     }
 }
 
