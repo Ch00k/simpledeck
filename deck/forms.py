23f7046b67c564e8fdfe511e92b20aac9b4ebad2
Andrii Yurchuk <ay@mntw.re>
2021-05-10 21:15:31 +0200

    Add theme attribute (#19)

--- deck/forms.py
+++ deck/forms.py
@@ -8,6 +8,7 @@ from .models import Deck
 class CreateForm(forms.Form):
     title = forms.CharField(label="Title", max_length=2048)
     text = forms.CharField(required=False, widget=forms.HiddenInput)
+    theme = forms.ChoiceField(choices=Deck.THEMES, widget=forms.Select)
 
 
 class DeleteForm(forms.ModelForm):
--- deck/models.py
+++ deck/models.py
@@ -4,6 +4,21 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class Deck(TimeStampedModel):
+    THEMES = [
+        ("black", "black"),
+        ("beige", "beige"),
+        ("blood", "blood"),
+        ("league", "league"),
+        ("moon", "moon"),
+        ("night", "night"),
+        ("serif", "serif"),
+        ("simple", "simple"),
+        ("sky", "sky"),
+        ("solarized", "solarized"),
+        ("white", "white"),
+    ]
+
     title = models.TextField()
     text = models.TextField()
+    theme = models.CharField(max_length=256, choices=THEMES, default="simple")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
