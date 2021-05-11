26bfbad98721d30ea129af7359523dc34f26873c
Andrii Yurchuk <ay@mntw.re>
2021-05-11 09:56:00 +0200

    Add demo page (#21)

--- simpledeck/views.py
+++ simpledeck/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from deck.models import Deck
+
 
 def handler404(request: HttpRequest, exception: Exception = None) -> HttpResponse:
     return render(request, "404.html")
+
+
+def demo(request: HttpRequest) -> HttpResponse:
+    return render(request, "demo.html", {"themes": Deck.THEMES})
